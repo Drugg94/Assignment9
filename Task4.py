5c84a914473b047b50cec068b4efba0b0a5aa93d
Derek <derek.ruggirello@gmail.com>
2022-03-25 00:01:06 -0400

    Trying to figure this out

--- Task4.py
+++ Task4.py
@@ -1,10 +1,18 @@
 # Revision 1 BEGIN/ 24Mar22
 ## Begin Derek Ruggirello here (24Mar22)
 
-sales_data = {}
+import csv
+import json
 
+sales_data = []
+csvDict = {"Transaction_date": "", "Product": "", "Price": "", "Payment_Type": "", "Name": "", "City": "", "State": "", "Country": ""}
 file = open("SalesJan2009.csv")
-
+csvDict = csv.DictReader(file, fieldnames="Transaction_date" "Product" "Price" "Payment_Type" "Name""City""State" "Country")
+jsonFile = open("transaction_data.json", "w")
+json.dump(csvDict, jsonFile)
+print(jsonFile.read())
+file.close()
+jsonFile.close()
 
 # Revision 1 FINAL 24Mar22
 ## End Derek Ruggirello here
